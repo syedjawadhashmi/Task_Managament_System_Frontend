a5ca1b1113bd1bb9c26bca397fc79291a6dde655
syedjawadhashmi <sjawad143@yahoo.com>
2019-03-06 01:41:59 +0500

    add firebase module

--- src/constant/api/firebase.js
+++ src/constant/api/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDj9i8jnZBL6igPuonwcSdKoQTdb0e6MnI",
-    authDomain: "labtask-2.firebaseapp.com",
-    databaseURL: "https://labtask-2.firebaseio.com",
-    projectId: "labtask-2",
-    storageBucket: "labtask-2.appspot.com",
-    messagingSenderId: "996188613125"
-  };
+  apiKey: "AIzaSyDj9i8jnZBL6igPuonwcSdKoQTdb0e6MnI",
+  authDomain: "labtask-2.firebaseapp.com",
+  databaseURL: "https://labtask-2.firebaseio.com",
+  projectId: "labtask-2",
+  storageBucket: "labtask-2.appspot.com",
+  messagingSenderId: "996188613125"
+};
 
 const firebaseApp = firebase.initializeApp(config);
 
